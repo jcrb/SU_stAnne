971d8d8823e6f33d4ba08dbe039056b4b76b3156
JCB <jeanclaude.bartier@gmail.com>
2014-01-05 12:45:05 +0100

    janvier

--- mes_fonctions.R
+++ mes_fonctions.R
@@ -9,6 +9,7 @@
 #'@param min mois de début (1-12)
 #'@param max mois de fin (1-12)
 #'@usage data<-sau("2013","stanne.csv")
+#'@maj 2014-01-05
 #'
 sau<-function(an="2013",hopNom="stanne.csv",min=1,max=12){
   an<-"2013"
@@ -19,8 +20,9 @@ sau<-function(an="2013",hopNom="stanne.csv",min=1,max=12){
     }
     else
       mois=as.character(i)
-    file<-paste(an,mois,hopNom,sep="_")
+    file<-paste(an, mois, hopNom, sep="_")
     print(file)
+    file <- paste("data", file, sep="/")
     f<-read.table(file,sep=",",header=TRUE,skip=2)
     if(i==1)
       data<-f
@@ -31,3 +33,24 @@ sau<-function(an="2013",hopNom="stanne.csv",min=1,max=12){
   data$date<-as.Date(data$date,format="%d/%m/%Y")
   return(data)
 }
+
+# Amélioration:
+# 
+# The first line paste the path where R must look at for the files. 
+# The second line creates an empty data object to store each of the importing files if any. 
+# The third line reads the path to the files, and then a loop for reading each existing file of type ".txt" as table. The last line in the loop creates the final table by appending each subset that was imported into memory. 
+# Finally, the last part of the program, which is out of the loop for efficiency purpose, simply write the final table to the disk as a text file, delimiting the columns by semicolon ';'.
+# 
+# path = "~/Documents/My Data/BRAZIL/Elections/"
+# out.file<-""
+# file.names <- dir(path, pattern =".txt")
+# for(i in 1:length(file.names)){
+#   file <- read.table(file.names[i],header=TRUE, sep=";", stringsAsFactors=FALSE)
+#   out.file <- rbind(out.file, file)
+# }
+#  write.table(out.file, file = "cand_Brazil.txt",sep=";", 
+#              row.names = FALSE, qmethod = "double",fileEncoding="windows-1252")
+# 
+# source: https://gist.github.com/danielmarcelino
+#              
+
